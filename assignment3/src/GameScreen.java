8f87b349522084f194509bc2843067a35030fe66
Kerem <118529271+keremuzer@users.noreply.github.com>
2024-04-27 17:00:20 +0300

    add character movement to A3

--- assignment3/src/GameScreen.java
+++ assignment3/src/GameScreen.java
@@ -1,15 +1,59 @@
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 
 public class GameScreen {
     private Scene scene;
     private Group root;
+    private Player player;
+    private AnimationTimer timer;
 
     public GameScreen() {
         this.root = new Group();
         this.scene = new Scene(root, 800, 600);
         scene.setFill(javafx.scene.paint.Paint.valueOf("#004873"));
-        root.getChildren().add(new Player());
+        player = new Player();
+        root.getChildren().add(player);
+        handleKeyPress();
+        handleKeyRelease();
+        startGame();
+    }
+
+    public void handleKeyPress() {
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case UP:
+                    player.setVelocity(0, -1.5);
+                    break;
+                case DOWN:
+                    player.setVelocity(0, 1.5);
+                    break;
+                case LEFT:
+                    player.setVelocity(-1.5, 0);
+                    break;
+                case RIGHT:
+                    player.setVelocity(1.5, 0);
+                    break;
+            }
+        });
+    }
+
+    public void handleKeyRelease() {
+        scene.setOnKeyReleased(e -> {
+            player.setVelocity(0, 0);
+        });
+    }
+
+    public void startGame() {
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                player.move();
+            }
+        };
+        timer.start();
     }
 
     public Scene getScene() {
--- assignment3/src/Player.java
+++ assignment3/src/Player.java
@@ -1,10 +1,33 @@
+import com.sun.javafx.scene.traversal.Direction;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class Player extends ImageView{
+public class Player extends ImageView {
+    private double x;
+    private double y;
+    private String leftImg;
+    private String rightImg;
+    private String upImg;
+    private String downImg;
+
     public Player() {
-        super(new Image("assets/drill/drill_01.png"));
+        this.leftImg = "assets/drill/drill_01.png";
+        this.rightImg = "assets/drill/drill_02.png";
+        this.upImg = "assets/drill/drill_03.png";
+        this.downImg = "assets/drill/drill_04.png";
+        setImage(new Image(leftImg));
         this.setX(400);
         this.setY(300);
+    }
+
+    public void move() {
+        this.setX(this.getX() + x);
+        this.setY(this.getY() + y);
+    }
+
+    public void setVelocity(double x, double y) {
+        this.x = x;
+        this.y = y;
+
     }
 }
