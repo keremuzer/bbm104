56316136e887ff1f959986b5b60de5fd86207122
Kerem <118529271+keremuzer@users.noreply.github.com>
2024-05-18 14:21:14 +0300

    add Stationery class to quiz 4

---
+++ quiz4/src/Stationery.java
@@ -0,0 +1,13 @@
+public class Stationery extends Item {
+    private String kind;
+
+    public Stationery(String name, String kind, int barcode, double price) {
+        super(name, barcode, price);
+        this.kind = kind;
+    }
+
+    @Override
+    public String toString() {
+        return "Kind of the " + getName() + " is " + kind + ". Its barcode is " + getBarcode() + " and its price is " + getPrice() + ".";
+    }
+}
