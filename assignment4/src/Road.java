4c6fb57df1778e4f0b18795c8e5bd14c0bb0ca14
Kerem <118529271+keremuzer@users.noreply.github.com>
2024-05-20 15:27:35 +0300

    add Road class to assignment 4

---
+++ assignment4/src/Road.java
@@ -0,0 +1,13 @@
+public class Road {
+    private String start;
+    private String end;
+    private int length;
+    private int id;
+
+    public Road(String start, String end, int length, int id) {
+        this.start = start;
+        this.end = end;
+        this.length = length;
+        this.id = id;
+    }
+}
