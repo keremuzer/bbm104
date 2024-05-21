d98adce92062e2375092acdd31f871c88659c5b8
Kerem <118529271+keremuzer@users.noreply.github.com>
2024-05-21 20:56:41 +0300

    add Route class to assignment 4

--- assignment4/src/Road.java
+++ assignment4/src/Road.java
@@ -1,13 +1,29 @@
 public class Road {
-    private String start;
-    private String end;
+    private String point1;
+    private String point2;
     private int length;
     private int id;
 
-    public Road(String start, String end, int length, int id) {
-        this.start = start;
-        this.end = end;
+    public Road(String point1, String point2, int length, int id) {
+        this.point1 = point1;
+        this.point2 = point2;
         this.length = length;
         this.id = id;
+    }
+
+    public String getPoint1() {
+        return point1;
+    }
+
+    public String getPoint2() {
+        return point2;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getId() {
+        return id;
     }
 }
---
+++ assignment4/src/Route.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Route {
+    private int distance;
+    private Road road;
+    private String point;
+    private ArrayList<Road> roadsTaken;
+
+    public Route(int distance, Road road, String point, ArrayList<Road> roadsTaken) {
+        this.distance = distance;
+        this.road = road;
+        this.point = point;
+        this.roadsTaken = new ArrayList<>(roadsTaken);
+        if (road != null) {
+            this.roadsTaken.add(road);
+        }
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Road getRoad() {
+        return road;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public ArrayList<Road> getRoadsTaken() {
+        return roadsTaken;
+    }
+}
